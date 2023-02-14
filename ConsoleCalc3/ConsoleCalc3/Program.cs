f8ec3fe082aa6fe7a0ccb34c33ba9356163c4f57
nikmorror37 <nikitamironchuk37@gmail.com>
2023-02-14 22:15:55 +0300

    commit3
    Add hometasks 1-5 from rep Lesson 2 to here

--- ConsoleCalc3/ConsoleCalc3/Program.cs
+++ ConsoleCalc3/ConsoleCalc3/Program.cs
@@ -7,6 +7,143 @@ namespace ConsoleCalc3
     {
         static void Main(string[] args)
         {
+            //ДЗ 1-5 было в репозитории Lesson2, еще сюда копипастну если не видели
+
+            /*      //задание ДЗ №5
+
+            Console.Write("Enter the color's number of rainbow: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Red");
+                    break;
+                case 2:
+                    Console.WriteLine("Orange");
+                    break;
+                case 3:
+                    Console.WriteLine("Yellow");
+                    break;
+                case 4:
+                    Console.WriteLine("Green");
+                    break;
+                case 5:
+                    Console.WriteLine("Light Blue");
+                    break;
+                case 6:
+                    Console.WriteLine("Blue");
+                    break;
+                case 7:
+                    Console.WriteLine("Violet");
+                    break;
+                default:
+                    Console.WriteLine("Write 1-7");
+                    break;
+            }
+
+            */
+
+
+            /*      //задание ДЗ №4
+
+            Console.Write("Enter the temperature: ");
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            if (t > -5)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (t <= -5 && t > -20)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (t <= -20)
+            {
+                Console.WriteLine("Cold");
+            }
+
+            */
+
+
+            /*      //задание ДЗ №3
+
+            Console.Write("Enter the number: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a %2 == 0)
+            {
+                Console.WriteLine("This number is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("This number is ODD");
+            }
+
+            */
+
+
+            /*      //задание ДЗ №2
+
+            Console.Write("Enter the number of month: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a == 1 || a == 2 || a == 12)
+            {
+                Console.WriteLine("Winter");
+            }
+            else if (a == 3 || a == 4 || a == 5) 
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (a == 6 || a == 7 || a == 8)
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (a == 9 || a == 10 || a == 11)
+            {
+                Console.WriteLine("Spring");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong number of month");
+            }
+
+            */
+
+
+            /*              //задание ДЗ №1
+            switch (a) 
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+                default:
+                    Console.WriteLine("Write 1-12");
+                    break;
+            }
+            */
             while (true)
             {
                 Console.WriteLine("\nHello, Nick's 1st console calculator welcomes you ! Read and than choose action \n");
